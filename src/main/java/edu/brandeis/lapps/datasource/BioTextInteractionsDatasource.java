beb405a4a9e41a48972c93fdce8ae907e9151bf6
marcverhagen <marc@cs.brandeis.edu>
2018-09-23 21:58:22 -0400

    Code import from GeneTagDatasource.

---
+++ src/main/java/edu/brandeis/lapps/datasource/BioTextInteractionsDatasource.java
@@ -0,0 +1,247 @@
+/*
+ * Copyright (c) 2018 Brandeis University
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+// This code is based on org.anc.lapps.datasource.generic.GenericDatasource, which
+// was originally taken from https://github.com/oanc/org.anc.lapps.datasource.generic
+
+package edu.brandeis.lapps.datasource;
+
+import java.io.BufferedReader;
+import org.lappsgrid.api.DataSource;
+import org.lappsgrid.discriminator.Discriminators;
+import org.lappsgrid.metadata.DataSourceMetadata;
+import org.lappsgrid.metadata.DataSourceMetadataBuilder;
+import org.lappsgrid.serialization.Data;
+import org.lappsgrid.serialization.Serializer;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import static org.lappsgrid.discriminator.Discriminators.Uri;
+
+import java.io.File;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.logging.Level;
+import org.anc.lapps.datasource.generic.Version;
+import org.lappsgrid.serialization.lif.Annotation;
+import org.lappsgrid.serialization.lif.Container;
+import org.lappsgrid.serialization.lif.Contains;
+import org.lappsgrid.serialization.lif.View;
+import org.lappsgrid.serialization.LifException;
+
+
+public class BioTextInteractionsDatasource implements DataSource
+{
+	private static final Logger logger = LoggerFactory.getLogger(BioTextInteractionsDatasource.class);
+
+	private final String path = "src/main/resources/data/";
+	private String metadata;
+	//private Map<String, File> index;
+	//private List<String> keys;
+	private String cachedError;
+
+
+	public BioTextInteractionsDatasource() {}
+
+
+	/**
+	 * Entry point for a Lappsgrid service.
+	 * <p>
+	 * Each service on the Lappsgrid will accept {@code org.lappsgrid.serialization.Data} object
+	 * and return a {@code Data} object with a {@code org.lappsgrid.serialization.lif.Container}
+	 * payload.
+	 * <p>
+	 * Errors and exceptions the occur during processing should be wrapped in a {@code Data}
+	 * object with the discriminator set to http://vocab.lappsgrid.org/ns/error
+	 * <p>
+	 * See <a href="https://lapps.github.io/org.lappsgrid.serialization/index.html?org/lappsgrid/serialization/Data.html>org.lappsgrid.serialization.Data</a><br />
+	 * See <a href="https://lapps.github.io/org.lappsgrid.serialization/index.html?org/lappsgrid/serialization/lif/Container.html>org.lappsgrid.serialization.lif.Container</a><br />
+	 *
+	 * @param input A JSON string representing a Data object
+	 * @return A JSON string containing a Data object with a Container payload.
+	 */
+	@Override
+	public String execute(String input)
+	{
+		if (cachedError != null)
+			return cachedError;
+		Data data = Serializer.parse(input, Data.class);
+		String discriminator = data.getDiscriminator();
+		if (Uri.ERROR.equals(discriminator))
+			return input;
+
+		//System.out.println("INPUT: " + input + "\n");
+
+		switch (discriminator)
+		{
+			case Uri.SIZE:
+
+				Data<Integer> sizeData = new Data<>();
+				sizeData.setDiscriminator(Uri.OK);
+				sizeData.setPayload(14996);
+				return Serializer.toJson(sizeData);
+
+			case Uri.LIST:
+
+				ArrayList<String> sentences = readSentences();
+				Data<java.util.List<String>> listData = new Data<>();
+				listData.setDiscriminator(Uri.STRING_LIST);
+				listData.setPayload(sentences);
+				return Serializer.toJson(listData);
+
+			case Uri.GET:
+
+				String key = data.getPayload().toString();
+				if (key == null)
+					return error("No key value provided");
+				else {
+					String fname = "/data/" + key.substring(0, 3) + File.separator
+							+ key.substring(0, 4) + File.separator + key;
+					String sentence = readSentence(fname);
+					try {
+						return createTextWithAnnotations(sentence); }
+					catch (LifException ex) {
+						return error(ex.getMessage()); }}
+
+			case Uri.GETMETADATA:
+
+				return metadata;
+
+			default:
+
+				String message = String.format("Invalid discriminator: %s, Uri.List is %s", discriminator, Uri.LIST);
+				//logger.warn(message);
+				return error(message);
+		}
+
+	}
+
+	/**
+	 * Returns a JSON string containing metadata describing the service. The
+	 * JSON <em>must</em> conform to the json-schema at
+	 * <a href="http://vocab.lappsgrid.org/schema/service-schema.json">http://vocab.lappsgrid.org/schema/service-schema.json</a>
+	 * (processing services) or
+	 * <a href="http://vocab.lappsgrid.org/schema/datasource-schema.json">http://vocab.lappsgrid.org/schema/datasource-schema.json</a>
+	 * (datasources).
+	 */
+	@Override
+	public String getMetadata()
+	{
+		if (metadata == null) {
+			DataSourceMetadata md = new DataSourceMetadataBuilder()
+					.name(this.getClass().getName())
+					.version(Version.getVersion())
+					.vendor("http:/www.lappsgrid.org")
+					.allow(Uri.ALL)
+					.encoding("UTF-8")
+					.format(Uri.LIF)
+					.description("GeneTag DataSource")
+					.license(Discriminators.Uri.APACHE2)
+					.build();
+			Data data = new Data();
+			data.setDiscriminator(Discriminators.Uri.META);
+			data.setPayload(md);
+			metadata = data.asPrettyJson();
+		}
+		return metadata;
+	}
+
+	protected String error(String message)
+	{
+		return new Data<>(Uri.ERROR, message).asPrettyJson();
+	}
+
+	/**
+	 * Return a Data object for the sentence and associated GENE annotations.
+	 * @param sentence
+	 * @return A Data object whose payload is a LIF object.
+	 * @throws LifException
+	 */
+	private String createTextWithAnnotations(String sentence) throws LifException
+	{
+		Container container = new Container();
+		int idx = sentence.indexOf('\n');
+		container.setText(sentence.substring(0, idx));
+		container.setLanguage("en");
+
+		String annotations = sentence.substring(idx).trim();
+		String[] annos = annotations.split("\n");
+
+		View view = container.newView();
+		view.addContains(Uri.NE, "GeneTag Gold Data", null);
+		Contains contains = view.getContains(Uri.NE);
+		contains.put("namedEntityCategorySet", "tags-ner-biomedical");
+		for (int i=0; i<annos.length; i++) {
+			String[] fields = annos[i].split("\t");
+			int p1 = Integer.parseInt(fields[0]);
+			int p2 = Integer.parseInt(fields[1]);
+			Annotation a = new Annotation("ne" + i, Uri.NE, p1, p2);
+			a.addFeature("category", "GENE");
+			view.addAnnotation(a); }
+
+		Data<Container> alldata = new Data<>(Uri.LIF, container);
+		return alldata.asPrettyJson();
+	}
+
+
+	/**
+	 * Return the content of a single file in the data directory, representing a
+	 * single sentence and it GENE annotations.
+	 */
+	private String readSentence(String fname)
+	{
+		StringBuilder buffer = new StringBuilder();
+		try {
+			BufferedReader reader = openResource(fname);
+			String line = null;
+			while((line = reader.readLine()) != null)
+				buffer.append(line).append("\n");
+		} catch (IOException ex) {
+			java.util.logging.Logger.getLogger(BioTextInteractionsDatasource.class.getName()).log(Level.SEVERE, null, ex);
+		}
+		return buffer.toString();
+	}
+
+	/**
+	 * Return a list of all sentence identifiers in /data/sentences.txt.
+	 */
+	private ArrayList<String> readSentences()
+	{
+		ArrayList<String> sentences = new ArrayList<>();
+		try {
+			BufferedReader reader = openResource("/data/sentences.txt");
+			String line;
+			line = reader.readLine();
+			sentences.add(line.trim());
+			while (true) {
+				line = reader.readLine();
+				if (line == null)
+					break;
+				sentences.add(line.trim()); }
+		} catch (IOException ex) {
+			java.util.logging.Logger.getLogger(BioTextInteractionsDatasource.class.getName()).log(Level.SEVERE, null, ex);
+		}
+		return sentences;
+	}
+
+	private BufferedReader openResource(String name)
+	{
+		InputStream stream = this.getClass().getResourceAsStream(name);
+		return new BufferedReader(new InputStreamReader(stream));
+	}
+}
---
+++ src/test/java/edu/brandeis/lapps/datasource/BioTextInteractionsDatasourceTest.java
@@ -0,0 +1,160 @@
+/*
+ * Copyright (c) 2018 Brandeis University
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package edu.brandeis.lapps.datasource;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import java.util.stream.Collectors;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+import org.json.simple.JSONObject;
+import org.json.simple.parser.JSONParser;
+import org.json.simple.parser.ParseException;
+
+import org.lappsgrid.discriminator.Discriminators;
+import org.lappsgrid.serialization.Data;
+import org.lappsgrid.serialization.Serializer;
+
+
+public class BioTextInteractionsDatasourceTest
+{
+	protected BioTextInteractionsDatasource datasource;
+	protected boolean verbose = false;
+
+	public BioTextInteractionsDatasourceTest() {}
+
+	@Before
+	public void setup()
+	{
+		datasource = new BioTextInteractionsDatasource();
+	}
+
+	@After
+	public void teardown()
+	{
+		datasource = null;
+	}
+
+	@Test
+	public void testSize()
+	{
+		// Testing whether we get the right kind of discriminator and the right number of
+		// entries in the index ("http://vocab.lappsgrid.org/ns/ok" and 14996 respectively)
+		Data data = new Data(Discriminators.Uri.SIZE);
+		String json = datasource.execute(data.asJson());
+		data = Serializer.parse(json, Data.class);
+		assertEquals("Invalid discriminator", Discriminators.Uri.OK, data.getDiscriminator());
+		assertEquals("Wrong number of entries", 14996, Integer.parseInt(data.getPayload().toString()));
+	}
+
+	@Test
+	public void testGet()
+	{
+		String key = "P00027739T0000";
+		Data dataIn = new Data(Discriminators.Uri.GET, key);
+		String result = datasource.execute(dataIn.asJson());
+		if (verbose)
+			System.out.println(result);
+
+		JSONObject json = parseJson(result);
+		JSONObject payload = (JSONObject) json.get("payload");
+		JSONObject text = (JSONObject) payload.get("text");
+		String value = (String) text.get("@value");
+		assertEquals("Wrong result for P00027739T0000", "gamma", value.substring(6, 11));
+
+		// I would really like to do the test using the serializer to parse the json string:
+		//Data dataOut = Serializer.parse(result, Data.class);
+		//Object pl = dataOut.getPayload();
+		// The problem is that I do not know how to deal with that Data class. It
+		// responds to the getPayLoad() message but I cannot find the code for that
+		// one anywhere. In this case it returns a java.util.LinkedHashMap, but I
+		// don't know whether I can rely on that.
+		//System.out.println(pl);
+		//System.out.println(pl.getClass());
+	}
+
+	@Test
+	public void testList()
+	{
+		Data data = new Data(Discriminators.Uri.LIST);
+		String json = datasource.execute(data.asPrettyJson());
+		JSONObject result = parseJson(json);
+		ArrayList list = (ArrayList) result.get("payload");
+		assertEquals("Wrong size of list", 14996, list.size());
+	}
+
+	//@Test
+	public void testReadLines() throws IOException
+	{
+		BufferedReader reader = open("/sentences.txt");
+		String line = reader.readLine();
+		System.out.println(line);
+		while (line != null) {
+			System.out.println(line);
+			line = reader.readLine();
+		}
+
+	}
+
+	//@Test
+	public void testCollectStream() throws IOException
+	{
+		BufferedReader reader = open("/sentences.txt");
+		String content = reader.lines().collect(Collectors.joining("\n"));
+		System.out.println(content);
+	}
+
+	//@Test
+	public void testForEachLine() throws IOException
+	{
+		BufferedReader reader = open("/sentences.txt");
+		reader.lines().forEach(System.out::println);
+	}
+
+	private BufferedReader open(String name) {
+		InputStream stream = this.getClass().getResourceAsStream("/sentences.txt");
+		assertNotNull(stream);
+		return new BufferedReader(new InputStreamReader(stream));
+	}
+
+	/**
+	 * Utility method for simple JSON parsing.
+	 * @param jsonString
+	 * @return A JSONObject for the input string
+	 */
+	private JSONObject parseJson(String jsonString) {
+		JSONObject result;
+		try {
+			JSONParser parser = new JSONParser();
+			result = (JSONObject) parser.parse(jsonString);
+		} catch (ParseException ex) {
+			Logger.getLogger(BioTextInteractionsDatasourceTest.class.getName()).log(Level.SEVERE, null, ex);
+			result = null;
+		}
+		return result;
+	}
+
+}
